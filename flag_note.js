a4b9586732d6aaa04c4195bc8e6480d2d6075817
Gino Pilotino <gunzip@25151.no-reply.drupal.org>
2011-01-24 10:13:47 +0000

    #1029918 Make Flag Notes exportable via Features by rickvug

--- flag_note.js
+++ flag_note.js
@@ -1,35 +1,16 @@
 ;if (Drupal && Drupal.jsEnabled) {
   Drupal.behaviors.flag_note = function(context) {
 
-    var $cbs = $('input[name=link_type]');
-    if (!$cbs.length) {
-      return ;
-    }
-    var fid = 'edit-link-type-flag-note-form';
-    var $fcb = $('#'+ fid);
-    var $fopts = $('#flag-note-options');
-    if (!$fcb.is(':checked')) {
-      $fopts.hide();
-    }
-    $cbs.bind('click', function(e) {
-      if ($(this).attr('id') == fid) {
-        $fopts.slideDown();
-      }
-      else {
-        $fopts.slideUp();
-      }
-    });
-
-    $vocSelect = $('#edit-flag-note-flag-note-vocab');
-    $vocOpts = $('#edit-flag-note-flag-note-vocab-label-wrapper, '+
-                 '#edit-flag-note-flag-note-vocab-help-wrapper, '+
+    $vocSelect = $('#edit-flag-note-vocab');
+    $vocOpts = $('#edit-flag-note-vocab-label-wrapper, '+
+                 '#edit-flag-note-vocab-help-wrapper, '+
                  '#edit-flag-note-hide-text-wrapper');
 
     if ($vocSelect.val() == 0) {
       $vocOpts.hide();
     }
 
-    $('#edit-flag-note-flag-note-vocab').change(function(e){
+    $vocSelect.change(function(e){
       if ($(this).val() > 0) {
         $vocOpts.slideDown();
       }
